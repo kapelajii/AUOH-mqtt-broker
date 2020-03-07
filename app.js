9d881444ab27d395fca2983a6680bc2e9bdf7d41
FeikkoDev <you@example.com>
2020-03-07 13:46:11 +0200

    First commit -addedwebsite and app

---
+++ app.js
@@ -0,0 +1,15 @@
+const aedes = require('./aedes')()
+const httpServer = require('http').createServer()
+const ws = require('websocket-stream')
+const port = process.env.PORT || 8081;
+
+//----------------------------
+// MQTT server over WebSocket
+//---------------------------
+ws.createServer({
+     server: httpServer 
+}, aedes.handle)
+
+httpServer.listen(port, function () {
+  console.log('websocket server listening on port ', port)
+})
